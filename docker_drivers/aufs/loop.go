57f63fba66ebe3710bd3ca51bf85102e6f2d5c04
Will Martin <wmartin@pivotal.io>
2016-11-23 10:03:53 +0000

    WIP
    Signed-off-by: Ed King <eking@pivotal.io>

--- docker_drivers/aufs/loop.go
+++ docker_drivers/aufs/loop.go
@@ -2,6 +2,7 @@ package aufs
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 
 	"code.cloudfoundry.org/lager"
@@ -14,6 +15,8 @@ type Loop struct {
 
 func (lm *Loop) MountFile(filePath, destPath string) error {
 	log := lm.Logger.Session("mount-file", lager.Data{"filePath": filePath, "destPath": destPath})
+	log.Info(fmt.Sprintf("Start mounting %s on %s", filePath, destPath))
+	defer log.Info(fmt.Sprintf("End mounting %s on %s", filePath, destPath))
 
 	output, err := exec.Command("mount", "-n", "-t", "ext4", "-o", "loop,noatime",
 		filePath, destPath).CombinedOutput()
@@ -26,7 +29,10 @@ func (lm *Loop) MountFile(filePath, destPath string) error {
 }
 
 func (lm *Loop) Unmount(path string) error {
+	contents, _ := ioutil.ReadFile("/proc/mounts")
 	log := lm.Logger.Session("unmount", lager.Data{"path": path})
+	log.Info(fmt.Sprintf("Start unmounting %s, mounts are: %s", path, contents))
+	defer log.Info(fmt.Sprintf("End unmounting %s", path))
 
 	var output []byte
 	err := lm.Retrier.Run(func() error {
@@ -37,6 +43,7 @@ func (lm *Loop) Unmount(path string) error {
 				// if it's not a mountpoint then this is fine
 				return nil
 			}
+			log.Error(fmt.Sprintf("Error unmounting %s", path), err)
 			return err
 		}
 		return nil
@@ -44,7 +51,7 @@ func (lm *Loop) Unmount(path string) error {
 
 	if err != nil {
 		log.Error("unmounting", err, lager.Data{"output": string(output)})
-		return fmt.Errorf("unmounting file: %s", err)
+		return fmt.Errorf("unmounting file (%s): %s", path, err)
 	}
 
 	return nil
